d9902e20956bf095e8432dd63359d8ad2a22565f
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2024-12-03 00:25:30 -0500

    First commit

---
+++ config/db.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+});
+
+
+let sql = "SELECT * From posts;";
+
+
+
+module.exports = pool.promise();
